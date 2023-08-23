3f4f54ab5ac858e18fcb8c0ab0ac3ed01902d854
vekariyasagar54 <113746663+vekariyasagar54@users.noreply.github.com>
2023-08-23 17:10:00 +0530

    Add files via upload

---
+++ Recursion/RateInMaze.cpp
@@ -0,0 +1,112 @@
+
+
+
+// Rat in a Maze Problem - I
+// MediumAccuracy: 35.75%Submissions: 204K+Points: 4
+// Win from a prize pool of INR 15K and get exciting merch! Register your team for Hack-A-Thon today!
+
+// Consider a rat placed at (0, 0) in a square matrix of order N * N. It has to reach the destination at (N - 1, N - 1). Find all possible paths that the rat can take to reach from source to destination. The directions in which the rat can move are 'U'(up), 'D'(down), 'L' (left), 'R' (right). Value 0 at a cell in the matrix represents that it is blocked and rat cannot move to it while value 1 at a cell in the matrix represents that rat can be travel through it.
+// Note: In a path, no cell can be visited more than one time. If the source cell is 0, the rat cannot move to any other cell.
+
+// Example 1:
+
+// Input:
+// N = 4
+// m[][] = {{1, 0, 0, 0},
+//          {1, 1, 0, 1},
+//          {1, 1, 0, 0},
+//          {0, 1, 1, 1}}
+// Output:
+// DDRDRR DRDDRR
+// Explanation:
+// The rat can reach the destination at
+// (3, 3) from (0, 0) by two paths - DRDDRR
+// and DDRDRR, when printed in sorted order
+// we get DDRDRR DRDDRR.
+// Example 2:
+// Input:
+// N = 2
+// m[][] = {{1, 0},
+//          {1, 0}}
+// Output:
+// -1
+// Explanation:
+// No path exists and destination cell is
+// blocked.
+// Your Task:
+// You don't need to read input or print anything. Complete the function printPath() which takes N and 2D array m[ ][ ] as input parameters and returns the list of paths in lexicographically increasing order.
+// Note: In case of no path, return an empty list. The driver will output "-1" automatically.
+
+// Expected Time Complexity: O((3N^2)).
+// Expected Auxiliary Space: O(L * X), L = length of the path, X = number of paths.
+
+// Constraints:
+// 2 ≤ N ≤ 5
+// 0 ≤ m[i][j] ≤ 1
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+  void findPathHelper(int i, int j, vector < vector < int >> & a, int n, vector < string > & ans, string move,
+                      vector < vector < int >> & vis) {
+    if (i == n - 1 && j == n - 1) {
+      ans.push_back(move);
+      return;
+    }
+
+    // downward
+    if (i + 1 < n && !vis[i + 1][j] && a[i + 1][j] == 1) {
+      vis[i][j] = 1;
+      findPathHelper(i + 1, j, a, n, ans, move + 'D', vis);
+      vis[i][j] = 0;
+    }
+
+    // left
+    if (j - 1 >= 0 && !vis[i][j - 1] && a[i][j - 1] == 1) {
+      vis[i][j] = 1;
+      findPathHelper(i, j - 1, a, n, ans, move + 'L', vis);
+      vis[i][j] = 0;
+    }
+
+    // right
+    if (j + 1 < n && !vis[i][j + 1] && a[i][j + 1] == 1) {
+      vis[i][j] = 1;
+      findPathHelper(i, j + 1, a, n, ans, move + 'R', vis);
+      vis[i][j] = 0;
+    }
+
+    // upward
+    if (i - 1 >= 0 && !vis[i - 1][j] && a[i - 1][j] == 1) {
+      vis[i][j] = 1;
+      findPathHelper(i - 1, j, a, n, ans, move + 'U', vis);
+      vis[i][j] = 0;
+    }
+
+  }
+public:
+  vector < string > findPath(vector < vector < int >> & m, int n) {
+    vector < string > ans;
+    vector < vector < int >> vis(n, vector < int > (n, 0));
+
+    if (m[0][0] == 1) findPathHelper(0, 0, m, n, ans, "", vis);
+    return ans;
+  }
+};
+
+int main() {
+  int n = 4;
+
+  vector < vector < int >> m = {{1, 0, 0, 0}, {1, 1, 0, 1}, {1, 1, 0, 0}, {0, 1, 1, 1}};
+
+  Solution obj;
+  vector < string > result = obj.findPath(m, n);
+  if (result.size() == 0)
+    cout << -1;
+  else
+    for (int i = 0; i < result.size(); i++) cout << result[i] << " ";
+  cout << endl;
+
+  return 0;
+}
